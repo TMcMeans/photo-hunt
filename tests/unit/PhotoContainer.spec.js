95605f81c3495c65200d6a488db204a00b1b885e
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-02-25 15:33:03 -0700

    add tests for Photo

---
+++ tests/unit/PhotoContainer.spec.js
@@ -0,0 +1,6 @@
+import { mount, shallowMount } from '@vue/test-utils';
+import PhotoContainer from '@/components/PhotoContainer';
+
+describe('PhotoContainer', () => {
+  it('should render a snapshot with correct data passed in', () => {});
+});
---
+++ tests/unit/SearchForm.spec.js
@@ -0,0 +1,6 @@
+import { mount, shallowMount } from '@vue/test-utils';
+import SearchForm from '@/components/SearchForm';
+
+describe('SearchForm', () => {
+  it('should render a snapshot with correct data passed in', () => {});
+});
